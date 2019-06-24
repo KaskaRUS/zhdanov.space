8b61235ec185ab446a9b3c2e87f3d2817be5cf08
Zhdnaov NIkolai <nikolai@zhdanov.space>
2019-06-24 23:06:45 +0300

    add simple page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # zhdanov.space
 Personal site of Zhdanov Nikolai
 
-For build needs jar of pixikt library in directory project/libs. https://gitlab.com/pixikt/pixikt
+For build needs jar of pixikt library in directory project/frontend/libs. https://gitlab.com/pixikt/pixikt
--- frontend/src/main/kotlin/space/zhdanov/web/frontend/main.kt
+++ frontend/src/main/kotlin/space/zhdanov/web/frontend/main.kt
@@ -1,9 +1,25 @@
 package space.zhdanov.web.frontend
 
-import space.zhdanov.web.entities.Star
+import kotlin.browser.document
+import kotlin.js.json
 
 fun main() {
-    val star = Star(1, 100.5, 100.3, 1.56, 0xFF34FA)
 
-    println(star)
+    val size = getSizeCanvas(
+        width = document.defaultView!!.innerWidth,
+        height = document.defaultView!!.innerHeight
+    )
+
+    val app = PIXI.Application(size.width, size.height, json(
+        "backgroundColor" to 0x0
+    ))
+    document.body!!.appendChild(app.view)
 }
+
+fun getSizeCanvas(width: Int, height: Int) =
+    if (width < height)
+        Size(1000 * (width / height.toFloat()), 1000f)
+    else
+        Size(1000f, 1000 * (height / width.toFloat()))
+
+data class Size(val width: Float, val height: Float)
