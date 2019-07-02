29550706b41a127e92ecdeda4c4a08bf377320ba
Zhdnaov NIkolai <nikolai@zhdanov.space>
2019-07-02 22:47:26 +0300

    add generator random stars

--- backend/src/main/kotlin/space/zhdanov/backend/controllers/StarsController.kt
+++ backend/src/main/kotlin/space/zhdanov/backend/controllers/StarsController.kt
@@ -3,6 +3,7 @@ package space.zhdanov.backend.controllers
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import space.zhdanov.web.entities.Star
@@ -14,16 +15,21 @@ import kotlin.random.Random
 open class StarsController {
 
     @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun background(): Flux<Star> =
-        Flux.interval(Duration.ofMillis(1000))
-            .take(10)
+    fun background(
+        @RequestParam width: Int,
+        @RequestParam height: Int,
+        @RequestParam("max_size") maxSize: Double,
+        @RequestParam(defaultValue = "300") count: Long
+    ): Flux<Star> =
+        Flux.interval(Duration.ofMillis(100))
+            .take(count)
             .map {
                 Star(
-                    x = Random.nextDouble(),
-                    y = Random.nextDouble(),
+                    x = (Random.nextDouble() - 0.5) * 2 * width,
+                    y = (Random.nextDouble() - 0.5) * 2 * height,
                     id = it,
-                    size = Random.nextDouble(),
-                    color = Random.nextInt()
+                    size = Random.nextDouble() * maxSize,
+                    color = 0xFFFFFF
                 )
             }
 }
